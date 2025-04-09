b4679eeebb30dd1d63e940608b58f0306b7e02d3
RD-rosh <you@example.com>
2025-04-09 18:38:24 +0530

    update_code_cleanup

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-
-
 const uri = process.env.MONGODB_URI;
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 async function run() {
@@ -25,14 +23,13 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
 app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
   }
 );
-  
-
   
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
