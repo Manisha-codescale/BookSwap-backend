62f96561b52d7c459cda0bc969371da332cc7b27
maneesha <manisha@codescale.lk>
2025-04-09 22:45:37 +0530

    Update Book Module

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
+import mongoose from 'mongoose';
+import bookRoute from './routes/bookRoute.js';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
   }
-  );
+);
+
+app.use('/api/book', bookRoute);
+
+mongoose.connect(MONGODB_URI)
+.then(() => {
+  console.log("Connected to the database.!");
+})
+.catch(() => {
+  console.log("Connection Failed.");
+})
   
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
   }
-  );
+);
