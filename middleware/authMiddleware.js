31a69d5451d1831938c2a68b021456e99c09000f
maneesha <manisha@codescale.lk>
2025-04-22 16:04:38 +0530

    AddedBookApi

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -12,19 +12,21 @@ if (!admin.apps.length) {
 }
 
 const authenticateFirebase = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.split(' ')[1];
-
-  if (!token) return res.status(401).json({ message: 'No token provided' });
-
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(token);
-    req.user = decodedToken;
-    next();
-  } catch (err) {
-    console.error('Token verification failed:', err);
-    return res.status(403).json({ message: 'Unauthorized' });
-  }
+   const authHeader = req.headers.authorization;
+   const token = authHeader?.split(' ')[1];
+  
+   if (!token) return res.status(401).json({ message: 'No token provided' });
+  
+   try {
+      const decodedToken = await admin.auth().verifyIdToken(token);
+      req.user = decodedToken;             
+      req.firebaseUID = decodedToken.uid;   
+      next();
+   } catch (err) {
+      console.error('Token verification failed:', err);
+      return res.status(403).json({ message: 'Unauthorized' });
+}
 };
+  
 
 export default authenticateFirebase;
